cb664851c5d80f6b5fb6c6835a49b373c01e6402
Vinicius Eduardo Agnes <114628323+ViniciusAgnes@users.noreply.github.com>
2023-10-17 21:15:35 -0300

    AGORA VAI ESSA POHA

--- script/Equipes.js
+++ script/Equipes.js
@@ -1,4 +1,4 @@
-const URL = "https://7ecl58ro.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%3D%3D%22equipe%22%5D&perspective=published";
+const URL = "https://7ecl58ro.api.sanity.io/v2021-10-21/data/query/production?query=+++*%5B_type%3D%3D%22equipe%22%5D%7B%0A++++%22id%22%3A_id%2C%0A++++++nome%2C%0A++++++cargo%2C%0A++++++%22imagem%22%3A+imagem.asset-%3Eurl%2C%0A++++++%22imagemresponsiva%22%3A+imagemresponsiva.asset-%3Eurl%2C%0A++%7D&perspective=published";
 
 const profs = document.getElementById("sep");
 
@@ -16,30 +16,9 @@ fetch(URL, {
             profs.appendChild(profsDiv);
 
             var ImgTag = document.createElement("img");
-            profs.appendChild(ImgTag);
-            var sourceElement = document.createElement('source');
-            sourceElement.setAttribute('media', '(max-width: 500px)');
-            if(element.imagem){
-                sourceElement.setAttribute('srcset', element.imagem.asset._ref);
-            }
-            ImgTag.appendChild(sourceElement);
-
-            var imgElement = document.createElement("img");
-            imgElement.classList.add("img-Equipe");
-            if(element.imagem){
-                imgElement.src = element.imagem.asset._ref;
-            }
-            pictureTag.appendChild(imgElement);
-
-            var tituloElement = document.createElement("p");
-            tituloElement.classList.add("nome");
-            tituloElement.innerText = element.nome; 
-            profsDiv.appendChild(tituloElement);
-
-            var tituloElement = document.createElement("p");
-            tituloElement.classList.add("cargo");
-            tituloElement.innerText = element.cargo; 
-            profsDiv.appendChild(tituloElement);
+            
+            ImgTag.src= element.imagem;
+            profsDiv.appendChild(ImgTag);
 
           
         })
